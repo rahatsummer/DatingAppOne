09ed1b2e2a80ea726073ef527445635d302d06b5
rahat <rahatsummer@gmail.com>
2020-10-10 07:51:04 +0600

    AutoMapper Calculate Age Problem

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -15,11 +15,14 @@ namespace DatingApp.API.Helpers
                  opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
              })
              .ForMember(dest => dest.Age, opt => {
-                 opt.ResolveUsing()
-             })
+                 opt.MapFrom(d => d.DateOfBirth.CalculateAge());
+             });
             CreateMap<tblUser, UserForDetailDto>()
                .ForMember(dest => dest.PhotoUrl, opt => {
                  opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+             })
+                .ForMember(dest => dest.Age, opt => {
+                 opt.MapFrom(d => d.DateOfBirth.CalculateAge());
              });
             CreateMap<tblPhoto, PhotosForDetailedDto>();
         }
